8a59e0a26472d9b8f166737653423a8d7df6ed9c
Pystronic <pystronic@gmail.com>
2025-01-21 20:26:38 +0100

    Fix install python version

--- install_perfkit.sh
+++ install_perfkit.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-# Note: Requires Python 3 to be installed
+# Note: Requires Python 3.11+ to be installed
+# Python version 3.13 is not supported, at the time of writing
 # Download and create venv
 git clone https://github.com/GoogleCloudPlatform/PerfKitBenchmarker.git
-python -m venv PerfKitBenchmarker/.venv
+python3.12 -m venv PerfKitBenchmarker/.venv
 source PerfKitBenchmarker/.venv/bin/activate
 
 # Install generall and AWS dependencies
-pip install -r PerfKitBenchmarker/requirements.txt
-pip install -r PerfKitBenchmarker/perfkitbenchmarker/providers/aws/requirements.txt
+pip3.12 install -r PerfKitBenchmarker/requirements.txt
+pip3.12 install -r PerfKitBenchmarker/perfkitbenchmarker/providers/aws/requirements.txt
